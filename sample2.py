5de0091da6d43dcf6460a33457539902806c00b3
Barry Carter <github@barrycarter.info>
2023-03-27 08:33:39 -0600

    samples for https://www.reddit.com/r/CodingHelp/comments/123nmpo/any_way_to_speed_up_npvectorize/

---
+++ sample2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+lats = -np.arange(-90, 90, 180./1800)-180./1800/2
+lngs = np.arange(-180, 180, 360./900)+360./900/2
+
+# convert to radians
+
+lats = lats/180*np.pi
+lngs = lngs/180*np.pi
+
+# meshgrid so we have lng and lat for each pixel
+
+lngs, lats = np.meshgrid(lngs, lats)
+
+# project these points into 3D space assuming Earth is a sphere; the unit here is Earth radii, so the radius of the sphere is 1 (for now)
+
+x = np.cos(lats)*np.cos(lngs)
+y = np.cos(lats)*np.sin(lngs)
+z = np.sin(lats)
+
